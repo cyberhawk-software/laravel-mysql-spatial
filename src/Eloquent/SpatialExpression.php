bfd8eeb01d9f0359a0094d40d2f2c8d23cd4a155
James <James.lomax@thecyberhawk.com>
2024-11-26 14:22:03 +0000

    Support Laravel 11

--- src/Eloquent/SpatialExpression.php
+++ src/Eloquent/SpatialExpression.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Query\Expression;
 
 class SpatialExpression extends Expression
 {
+    #[\ReturnTypeWillChange]
     public function getValue()
     {
         return "ST_GeomFromText(?, ?, 'axis-order=long-lat')";
